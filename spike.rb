2c65ca8649a641b009d2fe5dd5701f09ee54b20d
Larry Karnowski <larry@hickorywind.org>
2011-04-06 12:46:49 -0400

    Initial commit.

---
+++ spike.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'swish'
+
+(2..10).to_a.each do |page|
+  Dribbble::Shot.popular(:page => page, :per_page => 30).each do |shot|
+    print "Downloading shot #{shot.id}..."
+    File.open("./shots/shot-#{shot.id}.png", "wb") do |file| 
+      file.write Net::HTTP.get URI.parse(shot.image_url)
+    end
+    print" saved!\n"
+  end
+end
+
+puts "Done!"
