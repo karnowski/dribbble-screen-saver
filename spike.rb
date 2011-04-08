a84f449a3772ce00be53a2b599a51e2e821b4138
Larry Karnowski <larry@hickorywind.org>
2011-04-07 21:44:41 -0400

    Don't download a shot we've already downloaded.

--- spike.rb
+++ spike.rb
@@ -3,12 +3,17 @@
 require 'rubygems'
 require 'swish'
 
-# TODO: don't download a shot we already have
-
 (1..10).to_a.each do |page|
   Dribbble::Shot.popular(:page => page, :per_page => 30).each do |shot|
+    filename = "./shots/shot-#{shot.id}.png"
+    
+    if File.exist?(filename)
+      puts "#{filename} already exists!  Skipping."
+      next
+    end
+    
     print "Downloading shot #{shot.id}..."
-    File.open("./shots/shot-#{shot.id}.png", "wb") do |file| 
+    File.open(filename, "wb") do |file| 
       file.write Net::HTTP.get URI.parse(shot.image_url)
     end
     print" saved!\n"
