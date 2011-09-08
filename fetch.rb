e67d1832d7fa922297ff2a6b5f02d7d963e7705f
Larry Karnowski <larry@hickorywind.org>
2011-09-08 09:22:57 -0400

    Add players' usernames to the images (for easier tracking down of cool shots).

--- fetch.rb
+++ fetch.rb
@@ -13,14 +13,14 @@ end
 
 (1..10).to_a.each do |page|
   Dribbble::Shot.popular(:page => page, :per_page => 30).each do |shot|
-    filename = "./shots/shot-#{shot.id}.png"
-    
+    filename = "./shots/shot-#{shot.player.username}-#{shot.id}.png"
+
     if File.exist?(filename)
       puts "#{filename} already exists!  Skipping."
       next
     end
     
-    print "Downloading shot #{shot.id}..."
+    print "Downloading shot #{shot.id} by #{shot.player.username}..."
     File.open(filename, "wb") do |file| 
       file.write Net::HTTP.get URI.parse(shot.image_url)
     end
