8475fd4eb8f6d48ea2c41bd205832b026fbb8112
Larry Karnowski <larry@hickorywind.org>
2016-03-08 16:16:52 -0500

    Modernize to use new Dribbble API; requires API key

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "http://rubygems.org"
 
-gem "swish", "0.8.1"
-gem "httparty", "0.7.8"
+# gem "swish", "0.8.1"
+# gem "httparty", "0.6.1"
+gem "dribbble", "1.0.4"
--- fetch.rb
+++ fetch.rb
@@ -2,7 +2,11 @@
 
 require 'rubygems'
 require 'bundler/setup'
-require 'swish'
+require 'dribbble'
+require 'yaml'
+
+config = YAML.load_file('config.yml')
+token = config["token"]
 
 if ARGV.length > 0 && ARGV.first == "--new"
   require 'fileutils'
@@ -13,17 +17,19 @@ end
 
 (1..10).to_a.each do |page|
   puts "Reading page #{page} of 10..."
-  Dribbble::Shot.popular(:page => page, :per_page => 30).each do |shot|
-    filename = "./shots/shot-#{shot.player.username}-#{shot.id}.png"
+  Dribbble::Shot.all(token, :page => page, :per_page => 30).each do |shot|
+    username = shot.user["username"]
+    filename = "./shots/shot-#{username}-#{shot.id}.png"
+    image_url = shot.images["normal"]
 
     if File.exist?(filename)
       puts "#{filename} already exists!  Skipping."
       next
     end
     
-    print "Downloading shot #{shot.id} by #{shot.player.username}..."
+    print "Downloading shot #{shot.id} by #{username}..."
     File.open(filename, "wb") do |file| 
-      file.write Net::HTTP.get URI.parse(shot.image_url)
+      file.write Net::HTTP.get URI.parse(image_url)
     end
     print" saved!\n"
   end
